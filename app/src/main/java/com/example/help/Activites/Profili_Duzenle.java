665f7bb2889d599153a60b5809102a4143114211
Aleyna Karabacakoğlu <50250045+AleynaKarabacakoglu@users.noreply.github.com>
2020-03-25 00:44:57 +0300

    ilk commit

--- app/src/main/java/com/example/help/Activites/Profili_Duzenle.java
+++ app/src/main/java/com/example/help/Activites/Profili_Duzenle.java
@@ -12,6 +12,6 @@ public class Profili_Duzenle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profili__duzenle);
-        aa
+        aaa
     }
 }
